abfd0879066f60a6d5121df37f4f14500d725ac9
Vachounet <vachounet@live.fr>
2019-04-30 11:26:47 +0200

    add a build guard
    * for caf repo
    
    Change-Id: I4e4b8e94676341bb3bb0dd58836d1707a638dd5e
    Signed-off-by: xyyx <xyyx@mail.ru>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,6 @@
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
+ifeq ($(TARGET_WLAN_VARIANT),)
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
 include $(call all-subdir-makefiles)
+endif
 endif
